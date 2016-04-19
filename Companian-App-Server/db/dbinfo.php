f09eeb6b5a53a65af5747abe64cf335aa8fe832d
leador <Aa5w6d7w>
2016-04-19 10:30:00 +0200

    Upload Serverfiles

---
+++ Companian-App-Server/db/dbinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class DBInfo {
+
+    public $db;
+    public $base = "db_333479_2";
+    public $db_server = "ingamelandscape.lima-db.de";
+    public $db_user = "USER333479";
+    public $db_pass = ".WFWEas3";
+
+}
+
+?>
+
---
+++ Companian-App-Server/index.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    </head>
+    <body>
+
+
+        <div>
+            Userliste:
+            <?php
+            include_once("rpg.php");
+            $rpg = new RPG();
+            $rpg->listUser();
+            ?>
+        </div>
+        <div>
+            User erstellen:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="createuser">
+                <input type="text" name="username" value="username">
+                <input type="text" name="password"  value="password">
+                <input type="text" name="password2"  value="password2">
+                <input type="submit" name="submit" value="erstellen">
+            </form>
+        </div>
+
+        <div>
+            Login:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="loginuser">
+                <input type="text" name="username" value="username">
+                <input type="text" name="password" value="password">
+                <input type="submit" name="submit" value="login">
+            </form>
+        </div>
+
+        <div>
+            Team beitreten:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="selectteam">
+                <input type="text" name="id" value="ID">
+                <input type="text" name="team" value ="1 oder 2">
+                <input type="submit" name="submit" value="team beitreten">
+            </form>
+        </div>
+
+        <div>
+            Klasse wählen:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="selectclass">
+                <input type="text" name="id" value="ID">
+                <input type="text" name="class" value ="1 bis 12">
+                <input type="submit" name="submit" value="Klasse wählen">
+            </form>
+        </div>
+
+        <div>
+            Teams anzeigen:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="listteams">          
+                <input type="submit" name="submit" value="Teams anzeigen">
+            </form>
+        </div>
+        <div>
+            Klassen anzeigen:
+            <form action="user.php" method="POST">
+                <input type="hidden" name="action" value="listclasses">
+                <input type="text" name="team" value ="1 oder 2">
+                <input type="submit" name="submit" value="Klassen anzeigen">
+            </form>
+        </div>
+
+
+
+    </body>
+</html>
